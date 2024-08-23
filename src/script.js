a35b3a1516fc472779cf6623c2d9cbd416a54a78
S-mic <sashamicheldube@gmail.com>
2024-08-23 13:41:04 +0200

    wip on js

--- src/script.js
+++ src/script.js
@@ -19,7 +19,7 @@ function generatePoem(event) {
 
   let poemElement = document.querySelector("#poem");
   poemElement.classList.remove("hidden");
-  poemElement.innerHTML = `<div class="generating">⏳ Generating a French poem about ${instructionsInput.value}</div>`;
+  poemElement.innerHTML = `⌛ Please wait,generating a poem about ${userInstructions.value}`;
 
   axios.get(apiURL).then(displayPoem);
 }
