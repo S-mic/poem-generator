96c2eda6205a9680032d22dce750f3888bf59ed0
S-mic <sashamicheldube@gmail.com>
2024-08-24 10:03:58 +0200

    WIP

--- src/script.js
+++ src/script.js
@@ -11,7 +11,7 @@ function generatePoem(event) {
   event.preventDefault();
 
   let instructionsInput = document.querySelector("#user-instructions");
-  let apiKey = "2046c535afeb092fo82f1d306d8a2b2t";
+  let apiKey = "7co4b937aee7400aa625c575fb0tc301";
   let context =
     "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
   let prompt = `User instructions: Generate a French poem about ${instructionsInput.value}`;
@@ -19,7 +19,7 @@ function generatePoem(event) {
 
   let poemElement = document.querySelector("#poem");
   poemElement.classList.remove("hidden");
-  poemElement.innerHTML = `<div class="generating">⏳ Generating a French poem about ${instructionsInput.value}</div>`;
+  poemElement.innerHTML = `⌛ Please wait,generating a poem about ${instructionsInput.value}`;
 
   axios.get(apiURL).then(displayPoem);
 }
